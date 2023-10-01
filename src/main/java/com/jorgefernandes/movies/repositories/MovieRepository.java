1b64d28b5b8d93c0332625269fef19f828c6f7f8
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-10-01 19:19:26 +0100

    Implement movie entity, controller, repository and service.

---
+++ src/main/java/com/jorgefernandes/movies/repositories/MovieRepository.java
@@ -0,0 +1,16 @@
+package com.jorgefernandes.movies.repositories;
+
+import com.jorgefernandes.movies.domain.movie.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
---
+++ src/main/java/com/jorgefernandes/movies/services/MovieService.java
@@ -0,0 +1,25 @@
+package com.jorgefernandes.movies.services;
+
+import com.jorgefernandes.movies.domain.movie.Movie;
+import com.jorgefernandes.movies.repositories.MovieRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> allMovies() {
+        return this.repository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String  id) {
+        return this.repository.findMovieByImdbId(id);
+    }
+}
