95a26d3806f3eaf4e73195f3c3750af78634baa1
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-10-21 15:07:34 +0100

    Add CrossOrigin and Delete reviews.

--- src/main/java/com/jorgefernandes/movies/controllers/ReviewController.java
+++ src/main/java/com/jorgefernandes/movies/controllers/ReviewController.java
@@ -1,25 +1,46 @@
 package com.jorgefernandes.movies.controllers;
 
 import com.jorgefernandes.movies.domain.review.Review;
+import com.jorgefernandes.movies.repositories.ReviewRepository;
 import com.jorgefernandes.movies.services.ReviewService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+    @Autowired
+    private ReviewRepository repository;
+
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteReview(@RequestBody Map<String, String> request) {
+        String id = request.get("id");
+        if (id != null) {
+            Optional<Review> review = repository.findById(new ObjectId(id));
+            if (review.isPresent()) {
+                repository.deleteById(new ObjectId(id));
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 }
