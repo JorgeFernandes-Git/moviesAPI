1524e6c0eef1a40486245bb7c00b0a3103a48a92
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-10-21 19:24:34 +0100

    Add reviews mapping on backend.

--- src/main/java/com/jorgefernandes/movies/controllers/ReviewController.java
+++ src/main/java/com/jorgefernandes/movies/controllers/ReviewController.java
@@ -1,7 +1,5 @@
 package com.jorgefernandes.movies.controllers;
-
 import com.jorgefernandes.movies.domain.review.Review;
-import com.jorgefernandes.movies.repositories.ReviewRepository;
 import com.jorgefernandes.movies.services.ReviewService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -19,28 +17,36 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
-    @Autowired
-    private ReviewRepository repository;
-
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteReview(@RequestBody Map<String, String> request) {
-        String id = request.get("id");
-        if (id != null) {
-            Optional<Review> review = repository.findById(new ObjectId(id));
-            if (review.isPresent()) {
-                repository.deleteById(new ObjectId(id));
-                return ResponseEntity.noContent().build();
-            } else {
-                return ResponseEntity.notFound().build();
-            }
+    @GetMapping
+    public ResponseEntity<List<Review>> getAllReviews() {
+        return new ResponseEntity<>(reviewService.allReviews(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteReview(@PathVariable ObjectId id) {
+//        ObjectId reviewId = payload.get("id");
+
+        if (reviewService.deleteReview(id)) {
+            return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
         } else {
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>("Review not found or couldn't be deleted", HttpStatus.NOT_FOUND);
         }
     }
+
+//    @DeleteMapping
+//    public ResponseEntity<String> deleteReview(@RequestBody Map<String, ObjectId> payload) {
+//        ObjectId reviewId = payload.get("id");
+//
+//        if (reviewService.deleteReview(reviewId)) {
+//            return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>("Review not found or couldn't be deleted", HttpStatus.NOT_FOUND);
+//        }
+//    }
 
 }
--- src/main/java/com/jorgefernandes/movies/services/ReviewService.java
+++ src/main/java/com/jorgefernandes/movies/services/ReviewService.java
@@ -3,11 +3,14 @@ package com.jorgefernandes.movies.services;
 import com.jorgefernandes.movies.domain.movie.Movie;
 import com.jorgefernandes.movies.domain.review.Review;
 import com.jorgefernandes.movies.repositories.ReviewRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
@@ -16,6 +19,11 @@ public class ReviewService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
+    public List<Review> allReviews() {
+        return this.repository.findAll();
+    }
+
 
     public Review createReview(String reviewBody, String imdbId) {
         Review review = this.repository.insert(new Review(reviewBody));
@@ -26,5 +34,15 @@ public class ReviewService {
                 .first();
 
         return review;
+    }
+
+    public boolean deleteReview(ObjectId reviewId) {
+        Review review = this.repository.findById(reviewId).orElse(null);
+        if (review != null) {
+            this.repository.delete(review);
+            return true;
+        } else {
+            return false; // Review not found
+        }
     }
 }
